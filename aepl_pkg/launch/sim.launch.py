fed1d91e3bfee2a67a65ae3a5d8cc209c2c3dc12
carpit680 <carpit680@gmail.com>
2023-11-14 00:53:51 +0530

    launch file funct

---
+++ aepl_pkg/launch/sim.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                FindPackageShare("vrx_gz"), '/launch', '/competition.launch.py'])
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                FindPackageShare("vrx_gz"), '/launch', '/usv_joy_teleop.py'])
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                FindPackageShare("vrx_gazebo"), '/launch', '/rviz.launch.py'])
+        ),
+        Node(
+            package='aepl_pkg',  # Replace with your package name
+            executable='vrx_odom',  # Replace with your node executable
+            name='vrx_odom',
+            output='screen',
+        ),
+        Node(
+            package='aepl_pkg',  # Replace with your package name
+            executable='vrx_controller',  # Replace with your node executable
+            name='vrx_controller',
+            output='screen',
+        ),
+    ])
--- aepl_pkg/setup.py
+++ aepl_pkg/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['launch/sim.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +21,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'vrx_controller = aepl_pkg.vrx_controller:main'
+            'vrx_controller = aepl_pkg.vrx_controller:main',
             'vrx_odom = aepl_pkg.vrx_odom:main'
         ],
     },
