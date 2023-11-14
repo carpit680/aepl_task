189fc8f446c97f040727686518fc52dcd366e221
carpit680 <carpit680@gmail.com>
2023-11-14 18:02:47 +0530

    nav2 integrated

--- aepl_pkg/launch/sim.launch.py
+++ aepl_pkg/launch/sim.launch.py
@@ -1,11 +1,25 @@
+import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 def generate_launch_description():
+
+    params_file_arg = DeclareLaunchArgument(
+        'params_file',
+        default_value='/home/arpit/projects/aepl_ws/src/aepl_task/aepl_pkg/config/nav2_config.yaml',
+        description='Full path to the ROS2 parameters file to use for all launched nodes'
+    )
+
     return LaunchDescription([
+        params_file_arg,
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                FindPackageShare("nav2_bringup"), '/launch', '/navigation_launch.py'])
+        ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([
                 FindPackageShare("vrx_gz"), '/launch', '/competition.launch.py'])
